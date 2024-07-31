cf5f7cc109eff8c0bdbc8fd344bfd667767c3aba
Mokhirboy Muzaffarov <mokhirboy.muzaffarov@trackstreet.com>
2024-07-31 21:35:02 +0500

    added ping

--- main.py
+++ main.py
@@ -37,6 +37,10 @@ def preprocess_image(image_path, target_size):
 def index():
     return render_template('index.html')
 
+@app.route('/ping')
+def index():
+    print("pong")
+
 @app.route('/predict', methods=['POST'])
 def predict():
     if 'file' not in request.files:
